413b9cd1322366085dadb530f5f3f91f00f4a2e3
Yanpeng Chen <chenyanpeng1992@outlook.com>
2024-04-11 18:37:54 +0200

    Update get_parent_lineage.R

--- R/get_parent_lineage.R
+++ R/get_parent_lineage.R
@@ -36,47 +36,47 @@ get_parent_lineage <- function(group_label, deepth=0) {
     
     # get lineage tbl
     if ( label_level == "genus" ) {
-        lineage_info <- outline %>% filter(Current_genus == group_label) %>% 
-            distinct(Current_genus, .keep_all = TRUE) %>%
+        lineage_info <- outline %>% filter(Genus_current == group_label) %>% 
+            distinct(Genus_current, .keep_all = TRUE) %>%
             select(Kingdom, Phylum, Subphylum, Class, Subclass_current, Order_current, Family_current) %>%
             mutate(lineage=paste(Kingdom, Phylum, Subphylum, Class, Subclass_current, Order_current, Family_current, sep = ", ")) %>%
             select(lineage) %>% pull()
     }
     if ( label_level == "family" ) {
-        lineage_info <- outline %>% filter(Current_family == group_label) %>% 
-            distinct(Current_family, .keep_all = TRUE) %>%
+        lineage_info <- outline %>% filter(Family_current == group_label) %>% 
+            distinct(Family_current, .keep_all = TRUE) %>%
             mutate(lineage=paste(Kingdom, Phylum, Subphylum, Class, Subclass_current, Order_current, sep = ", ")) %>%
             select(lineage) %>% pull()
     }
     if ( label_level == "order" ) {
-        lineage_info <- outline %>% filter(Current_order == group_label) %>% 
-            distinct(Current_order, .keep_all = TRUE) %>%
+        lineage_info <- outline %>% filter(Order_current == group_label) %>% 
+            distinct(Order_current, .keep_all = TRUE) %>%
             mutate(lineage=paste(Kingdom, Phylum, Subphylum, Class, Subclass_current, sep = ", ")) %>%
             select(lineage) %>% pull()
     }
     if ( label_level == "subclass" ) {
-        lineage_info <- outline %>% filter(Current_subclass == group_label) %>% 
-            distinct(Current_subclass, .keep_all = TRUE) %>%
+        lineage_info <- outline %>% filter(Subclass_current == group_label) %>% 
+            distinct(Subclass_current, .keep_all = TRUE) %>%
             mutate(lineage=paste(Kingdom, Phylum, Subphylum, Class, sep = ", ")) %>%
             select(lineage) %>% pull()
     }
     if ( label_level == "class" ) {
-        lineage_info <- outline %>% filter(Current_class == group_label) %>% 
-            distinct(Current_class, .keep_all = TRUE) %>%  
+        lineage_info <- outline %>% filter(Class_current == group_label) %>% 
+            distinct(Class_current, .keep_all = TRUE) %>%  
             mutate(lineage=paste(Kingdom, Phylum, Subphylum, sep = ", ")) %>%
             select(lineage) %>% pull()
            
     }
     if ( label_level == "subphylum" ) {
-        lineage_info <- outline %>% filter(Current_subphylun == group_label) %>% 
-            distinct(Current_subphylun, .keep_all = TRUE) %>%  
+        lineage_info <- outline %>% filter(Subphylum_current == group_label) %>% 
+            distinct(Subphylum_current, .keep_all = TRUE) %>%  
             mutate(lineage=paste(Kingdom, Phylum, sep = ", ")) %>%
             select(lineage) %>% pull()
             
     }
     if ( label_level == "phylum" ) {
-        lineage_info <- outline %>% filter(Current_phylum == group_label) %>% 
-            distinct(Current_phylum, .keep_all = TRUE) %>%
+        lineage_info <- outline %>% filter(Phylum_current == group_label) %>% 
+            distinct(Phylum_current, .keep_all = TRUE) %>%
             mutate(lineage=paste(Kingdom, Phylum, sep = ", ")) %>%
             select(lineage) %>% pull()
     }
